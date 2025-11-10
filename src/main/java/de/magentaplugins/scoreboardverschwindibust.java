d8d01d7481ce1e842bd71205004986567dbbb06d
MagentaCoding <emergency.berlin@outlook.de>
2025-11-10 12:59:30 +0100

    Initial commit: kompletten aktuellen Ordner hochladen

---
+++ src/main/java/de/magentaplugins/scoreboardverschwindibust.java
@@ -0,0 +1,17 @@
+package de.magentaplugins;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class scoreboardverschwindibust extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
